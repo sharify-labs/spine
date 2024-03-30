d8a987eae6a280400b899b34fc3e8e770e4a6363
xEricL <37921711+xericl@users.noreply.github.com>
2024-03-30 01:55:01 -0400

    move server startup to main.go

--- main.go
+++ main.go
@@ -1,14 +1,91 @@
 package main
 
 import (
+	"context"
 	"embed"
-	"github.com/sharify-labs/spine/server"
+	"errors"
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/discord"
+	"github.com/sharify-labs/spine/clients"
+	"github.com/sharify-labs/spine/config"
+	"github.com/sharify-labs/spine/database"
+	"github.com/sharify-labs/spine/middleware"
+	"github.com/sharify-labs/spine/router"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 //go:embed assets/*
 var assets embed.FS
 var version string
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, _ echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
-	server.Start(assets, version)
+	// Load .env
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	// Initialize Sentry
+	//lib.StartSentry(version)
+
+	// Create app
+	e := echo.New()
+	e.Logger.SetLevel(log.Lvl(config.Int("LOG_LEVEL")))
+	e.IPExtractor = echo.ExtractIPFromXFFHeader() // internal IPs trusted by default
+	e.Renderer = &Template{
+		templates: template.Must(template.ParseFS(assets, "assets/templates/*.html")),
+	}
+
+	// Setup Goth Auth Providers
+	goth.UseProviders(
+		discord.New(
+			config.Str("DISCORD_CLIENT_ID"),
+			config.Str("DISCORD_CLIENT_SECRET"),
+			config.Str("DISCORD_CALLBACK_URL"),
+			"identify", "email",
+		),
+	)
+
+	clients.Setup()
+	database.Setup()
+	middleware.Setup(e, assets)
+	router.Setup(e)
+
+	// Setup graceful shutdown
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	// Start app
+	go func() {
+		fmt.Println("Started Spine v" + version)
+		if err := e.Start(":" + config.Str("PORT")); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			e.Logger.Fatalf("shutting down server: %v", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully close the server with a timeout of 30 seconds.
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	} else {
+		e.Logger.Info("server closed gracefully")
+	}
 }
